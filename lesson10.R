64188334439175f7cfa6b1bb19270feeb1418a46
jonkq <55402632+jonkq@users.noreply.github.com>
2019-12-12 19:40:53 +0100

    10

--- lesson10.R
+++ lesson10.R
@@ -7,18 +7,65 @@
 #'
 #' 1. Estimate an automated ARIMA model of Swiss GDP.
 #'
+library(tidyverse)
+library(rdbnomics)
+library(forecast)
+library(tsbox)
+library(seasonal)
+library(quantmod)
+library(prophet)
+library(prophet)
+
+swiss_gdp <-
+  rdb(ids = c(
+    "Eurostat/namq_10_gdp/Q.CLV05_MEUR.NSA.B1G.CH"
+  )) %>%
+  select(period, value) %>%
+  ts_ts()
+
+m_arima <- auto.arima(swiss_gdp)
 
 #' 2. Inspect the model: What is the degree of seasonal and non-seasonal
 #' differentiation, what are the seasonal and non-seasonal AR and MA orders?
 #'
+m_arima
+
+
+seas(swiss_gdp)
+
 
 #' 3. Re-estimate 1, using R base `arima()` function
 #'
+base_arima <- arima(swiss_gdp)
+base_arima
+
 
 #' 4. Seasonally adjust Swiss GDP. Compare the ARIMA model with the one from
 #' `auto.arima`.
 #'
+m_arima_adj <- auto.arima(swiss_gdp, seasonal = F)
+
 
 #' 5. Download your stock title of choice. Use prophet to forecast the series.
 #'
+nestle <-
+  getSymbols("NESN.SW", src = "yahoo", auto.assign = FALSE) %>%
+  ts_tbl() %>%
+  filter(id == "NESN.SW.Close")
+
+ts_plot(nestle)
+
+nestle <- nestle[,-1]
+colnames(nestle) <- c("ds","y")
+
+forecast_nestle <- prophet(nestle)
+
+future <- make_future_dataframe(forecast_nestle, periods = 365)
+forecast <- predict(forecast_nestle, future)
+
+plot(forecast_nestle, forecast)
+
+
+
+
 
